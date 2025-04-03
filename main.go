a450cecfd7c9e07f4069e6e20315d68f5482f630
kairat_satbekov <kairat_satbekov@epam.com>
2025-04-04 00:37:21 +0500

    add task 3

--- main.go
+++ main.go
@@ -44,12 +44,42 @@ func postTaskHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, newTask)
 }
 
+func updateTaskHandler(c echo.Context) error {
+	var task Task
+	id := c.Param("id")
+
+	var request requestBody
+	if err := c.Bind(&request); err != nil {
+		return c.JSON(http.StatusBadRequest, "Invalid patch request")
+	}
+
+	if err := db.First(&task, id).Error; err != nil {
+		return c.String(http.StatusNotFound, "Could not find task")
+	}
+	task.Task = request.Task
+	task.IsDone = request.IsDone
+
+	if err := db.Save(&task).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, "Could not update task")
+	}
+
+	return c.JSON(http.StatusOK, task)
+}
+
 func getTaskHandler(c echo.Context) error {
 	var tasks []Task
 	if err := db.Find(&tasks).Error; err != nil {
 		return c.String(http.StatusInternalServerError, "Could not get tasks")
 	}
 	return c.JSON(http.StatusOK, tasks)
+}
+
+func deleteTaskHandler(c echo.Context) error {
+	id := c.Param("id")
+	if err := db.Delete(&Task{}, id).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, "Could not delete task")
+	}
+	return c.JSON(http.StatusOK, "Task deleted")
 }
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
@@ -62,6 +92,8 @@ func main() {
 	e := echo.New()
 	e.POST("/task", postTaskHandler)
 	e.GET("/task", getTaskHandler)
+	e.PATCH("/task/:id", updateTaskHandler)
+	e.DELETE("/task/:id", deleteTaskHandler)
 
 	e.Logger.Fatal(e.Start(":9090"))
 }
